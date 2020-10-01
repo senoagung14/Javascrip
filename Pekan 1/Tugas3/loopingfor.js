fa1cd75900cac73874f4d5a62fb2a186db166c70
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-10-01 16:20:24 +0700

    add project

---
+++ Pekan 1/Tugas3/loopingfor.js
@@ -0,0 +1,18 @@
+console.log("Nomor 1");
+console.log();
+
+var bil = 0;
+
+console.log("LOOPING PERTAMA");
+while(bil<20){
+    bil+=2;
+    console.log(String(bil), " - I love coding");
+}
+
+console.log("LOOPING KEDUA");
+while(bil>0){
+    console.log(String(bil), " - I will become a mobile developer");
+    bil-=2;
+}
+
+console.log();
